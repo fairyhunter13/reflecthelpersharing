d629c38ac877d6bbc9a9fccade6549ff101ca3ac
fairyhunter13 <fairyhunter13@gmail.com>
2020-12-08 01:44:16 +0700

    Completing case 4 :sparkles:

--- case4/main.go
+++ case4/main.go
@@ -1,5 +1,14 @@
 package main
 
+import (
+	"fmt"
+	"reflect"
+	"strconv"
+
+	"github.com/fairyhunter13/reflecthelper/v3"
+)
+
+// Extract value with specific type from a variable of reflect.Value
 func main() {
 	normal()
 	usingReflectHelper()
@@ -7,10 +16,19 @@ func main() {
 
 func normal() {
 	// normally
-
+	testVal := reflect.ValueOf("1")
+	result, err := strconv.ParseBool(testVal.String())
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("The bool value of testVal:", result)
 }
 
 func usingReflectHelper() {
 	// using reflecthelper
-
+	result, err := reflecthelper.ExtractBool(reflect.ValueOf("false"))
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("The bool value of testVal:", result)
 }
