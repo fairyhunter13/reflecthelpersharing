706f5c2bae6f8ef58174dfe254b955d30e3e293d
fairyhunter13 <fairyhunter13@gmail.com>
2020-12-08 04:38:18 +0700

    Completing case 5 :sparkles:

--- case5/main.go
+++ case5/main.go
@@ -1,5 +1,12 @@
 package main
 
+import (
+	"fmt"
+	"reflect"
+
+	"github.com/fairyhunter13/reflecthelper/v3"
+)
+
 // Assignment of two variables of reflect.Value with different type
 func main() {
 	normal()
@@ -8,9 +15,26 @@ func main() {
 
 func normal() {
 	// normally
+	uintVal := uint64(65)
+	var intVal *int
+	hello := 0
+	intVal = &hello
+	assignerRef := reflect.ValueOf(&intVal).Elem().Elem()
+	valRef := reflect.ValueOf(uintVal)
+	assignerRef.SetInt(int64(valRef.Uint()))
+	fmt.Println("The intVal is:", *intVal)
 }
 
 func usingReflectHelper() {
 	// using reflecthelper
-
+	uintVal := uint64(65)
+	var intVal *int
+	err := reflecthelper.AssignReflect(
+		reflecthelper.GetInitChildElem(reflect.ValueOf(&intVal)),
+		reflect.ValueOf(uintVal),
+	)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("The intVal is:", *intVal)
 }
