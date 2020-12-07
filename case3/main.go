9ba584046cae587bb14dc8a30661f42cb77281cb
fairyhunter13 <fairyhunter13@gmail.com>
2020-12-08 01:31:34 +0700

    Completing case 3 :sparkles:

--- case3/main.go
+++ case3/main.go
@@ -1,5 +1,13 @@
 package main
 
+import (
+	"fmt"
+	"reflect"
+
+	"github.com/fairyhunter13/reflecthelper/v3"
+)
+
+// Get the child elem kind of a multi level pointer variable of reflect.Value
 func main() {
 	normal()
 	usingReflectHelper()
@@ -7,10 +15,14 @@ func main() {
 
 func normal() {
 	// normally
-
+	var test ***int
+	kindOfTest := reflect.ValueOf(&test).Type().Elem().Elem().Elem().Elem().Kind()
+	fmt.Println("The child elem kind of test is:", kindOfTest)
 }
 
 func usingReflectHelper() {
 	// using reflecthelper
-
+	var test ***int
+	kindOfTest := reflecthelper.GetChildElemPtrKind(reflect.ValueOf(&test))
+	fmt.Println("The child elem kind of test is:", kindOfTest)
 }
